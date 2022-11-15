a10dcb208eb871e8f45beee604102d80459ddf3d
BoYoung00 <101164065+BoYoung00@users.noreply.github.com>
2022-11-15 23:36:51 +0900

    백엔드 공부 11일차

--- src/main/java/user/UserDao.java
+++ src/main/java/user/UserDao.java
@@ -3,6 +3,9 @@ package user;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDao {
     private static String url = "jdbc:mysql://localhost:3306/userdatabase";
@@ -32,5 +35,37 @@ public class UserDao {
         }
         return count;
     }
+
+    public List<UserDto> getvalue() {
+        List<UserDto> list = new ArrayList<>();
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String sql = "select userid, userpassword from usertable order by userid desc";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            try (ResultSet rs = ps.executeQuery(sql)) {
+                while (rs.next()) {
+                    String userID = rs.getString(1);
+                    String userPassword = rs.getString(2);
+
+                    UserDto userdto = new UserDto(userID, userPassword);
+                    list.add(userdto);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
 }
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,9 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ page import="user.UserDto" %>
+<%@ page import="user.UserDao" %>
+<%@ page import="java.io.PrintWriter" %>
+<%@ page import="java.util.List" %>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,5 +15,29 @@
     <input type="password" name="userPassword">
     <input type="submit" value="회원가입">
 </form>
+
+<div>
+    <%
+        UserDao userDAO = new UserDao();
+        List<UserDto> list = userDAO.getvalue();
+
+        for (int i=0; i<list.size(); i++) {
+    %>
+    <table border="1px">
+        <thead>
+            <th>아이디</th>
+            <th>비밀번호</th>
+        </thead>
+        <tbody>
+        <tr>
+            <td><%=list.get(i).getUserID() %></td>
+            <td><%=list.get(i).getUserPassword()%></td>
+        </tr>
+        </tbody>
+    </table>
+    <%
+        }
+    %>
+</div>
 </body>
 </html>
